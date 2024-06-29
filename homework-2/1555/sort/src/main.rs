49cf21089a531ac849a8936285de0724890b7bae
fuu <fu@yihaofang.com>
2024-06-29 23:56:50 +0800

    1.improvement of sorting

--- homework-2/1555/sort/src/main.rs
+++ homework-2/1555/sort/src/main.rs
@@ -2,17 +2,20 @@ fn main() {
     println!("Homework_2 - task_1:");
 
     let mut test_1:Vec<i32>=vec![23,33,12,67,18,88,31];
-    sort_vector(&mut test_1)
+
+    let mut test_2:Vec<i32>=vec![1223,22334,1332,2567,1228,8118,3122,997,3382];
+    sort_vector(&mut test_1);
+    sort_vector(&mut test_2)
 }
 
 fn sort_vector(vec: &mut Vec<i32>) {
-    println!("****** Start of sorting ******" );
+    println!("\n****** Start of sorting ******" );
     println!("Orginal array: {:?}",vec );
     println!("-------------------" );
     let mut i = 0;
     let mut tmp:i32;
     while i < vec.len()-1 {
-        //println!("Index: {}",i);
+
         let mut j = 0;
         while j < vec.len()-i-1 {
             if vec[j+1] < vec[j] {
@@ -28,5 +31,5 @@ fn sort_vector(vec: &mut Vec<i32>) {
 
     println!("-------------------" );
     println!("Done: {:?}",vec );
-    println!("****** End of sorting ******" );
+    println!("****** End of sorting ******\n" );
 }
