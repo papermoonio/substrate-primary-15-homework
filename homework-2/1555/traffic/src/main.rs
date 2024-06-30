fab8c422a26a3bd44d5dc1a85e96485f88f71572
fuu <fu@yihaofang.com>
2024-06-30 09:28:41 +0800

    1.traffic light impl time done;

--- homework-2/1555/traffic/src/main.rs
+++ homework-2/1555/traffic/src/main.rs
@@ -1,3 +1,15 @@
+//use light::*;
+pub mod light;
+
+use light::*;
+
 fn main() {
-    println!("Hello, world!");
+    let red = Red{};
+    println!("Red ligth time: {}s", red.time());
+
+    let green = Green{};
+    println!("Green ligth time: {}s", green.time());
+
+    let yellow = Yellow{};
+    println!("Yellow ligth time: {}s", yellow.time());
 }
