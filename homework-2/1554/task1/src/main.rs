316e86612af40bd6f2d00bcbb39c44f032dee321
lucas <lichunyang@blued.com>
2024-07-05 18:06:39 +0800

    finish homework2

--- homework-2/1554/task1/src/main.rs
+++ homework-2/1554/task1/src/main.rs
@@ -1,3 +1,91 @@
+
+// basic fn
+pub fn bubble_sort(arr: &mut [i32]) {
+    if arr.len() <= 1 {
+        return;
+    }
+
+    let size = arr.len();
+    for i in 0..(size - 1) {
+        // 标记当前循环是否发生元素交换
+        let mut swapped = false;
+
+        // 最后i个元素已经排好了顺序
+        for j in 1..(size - i) {
+            if arr[j - 1] > arr[j] { 
+                arr.swap(j - 1, j);
+                swapped = true;
+            }
+        }
+
+        // 如果当前循环没有发生元素交换，说明数组已经有序
+        if !swapped {
+            break;
+        }
+    }
+} 
+
+// 进阶版 泛型的使用
+pub fn bubble_sort2<T: PartialOrd>(arr: &mut [T]) {
+    if arr.len() <= 1 {
+        return;
+    }
+
+    let size = arr.len();
+    for i in 0..(size - 1) {
+        // 标记当前循环是否发生元素交换
+        let mut swapped = false;
+
+        // 最后i个元素已经排好了顺序
+        for j in 1..(size - i) {
+            if arr[j - 1] > arr[j] { 
+                arr.swap(j - 1, j);
+                swapped = true;
+            }
+        }
+
+        // 如果当前循环没有发生元素交换，说明数组已经有序
+        if !swapped {
+            break;
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_number_vec() {
+        let mut vec = vec![7, 49, 73, 58, 30, 72, 44, 78, 23, 9];
+        bubble_sort(&mut vec);
+        assert_eq!(vec, vec![7, 9, 23, 30, 44, 49, 58, 72, 73, 78]);
+    }
+
+    #[test]
+    fn test_string_vec() {
+        let mut vec = vec![
+            String::from("Bob"),
+            String::from("David"),
+            String::from("Carol"),
+            String::from("Alice"),
+        ];
+        bubble_sort2(&mut vec);
+        assert_eq!(
+            vec,
+            vec![
+                String::from("Alice"),
+                String::from("Bob"),
+                String::from("Carol"),
+                String::from("David"),
+            ]
+        );
+    }
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
+
--- homework-2/1554/task2/src/main.rs
+++ homework-2/1554/task2/src/main.rs
@@ -1,3 +1,63 @@
+// 定义交通信号灯枚举
+enum TrafficLight {
+    Red,
+    Yellow,
+    Green,
+}
+
+// 为TrafficLight枚举实现time方法
+impl TrafficLight {
+    fn time(&self) -> u8 {
+        match self {
+            TrafficLight::Red => 60,    // 红灯持续60秒
+            TrafficLight::Yellow => 5,  // 黄灯持续5秒
+            TrafficLight::Green => 30,  // 绿灯持续30秒
+        }
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_green_30_seconds() {
+        // 创建一个绿灯实例
+        let light = TrafficLight::Green;
+        let green_time = light.time();
+        assert_eq!(green_time, 30);
+    }
+
+    #[test]
+    fn test_red_60_seconds() {
+        // 创建一个绿灯实例
+        let light = TrafficLight::Red;
+        let red_time = light.time();
+        assert_eq!(red_time, 60);
+    }
+
+    #[test]
+    fn test_yeallow_5_seconds() {
+        // 创建一个绿灯实例
+        let light = TrafficLight::Yellow;
+        let yeallow_time = light.time();
+        assert_eq!(yeallow_time, 5);
+    }
+
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    // 创建一个绿灯实例
+    let light = TrafficLight::Green;
+    
+    // 使用模式匹配来处理不同的交通信号灯
+    match light {
+        TrafficLight::Red => println!("STOP for {} seconds.", light.time()),
+        TrafficLight::Yellow => println!("CAUTION for {} seconds.", light.time()),
+        TrafficLight::Green => println!("GO for {} seconds.", light.time()),
+    }
 }
