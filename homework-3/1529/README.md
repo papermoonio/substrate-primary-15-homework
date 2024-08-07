7a94ed094e07274d594f0f47ec1ababfc95854f1
zhnliving <zhnliving@icloud.com>
2024-08-07 14:13:09 +0800

    补充转账功能

--- homework-3/1529/README.md
+++ homework-3/1529/README.md
@@ -6,6 +6,7 @@ node index.js create-account
 Mnemonic: chat spray dwarf oval unique fault shuffle good wet bracket law glance
 Address: 5E66A9vWvjSxwXeK7ZvpJLVjGbxPKmoKnEAF8HEzMaw3sLu8
 ```
+
 2. 查询账户余额
 ```
 node index.js check-balance 5E66A9vWvjSxwXeK7ZvpJLVjGbxPKmoKnEAF8HEzMaw3sLu8
@@ -13,3 +14,12 @@ node index.js check-balance 5E66A9vWvjSxwXeK7ZvpJLVjGbxPKmoKnEAF8HEzMaw3sLu8
 ```
 Balance of 5E66A9vWvjSxwXeK7ZvpJLVjGbxPKmoKnEAF8HEzMaw3sLu8: 0
 ```
+
+3. 转账到其他帐号
+```
+node index.js transfer 5EvuyBfdu2vXp5C24RXkgPVCcaYu2g7obHfx3MGf4E5vkdPB 5E66A9vWvjSxwXeK7ZvpJLVjGbxPKmoKnEAF8HEzMaw3sLu8 1
+```
+```
+Balance of 5E66A9vWvjSxwXeK7ZvpJLVjGbxPKmoKnEAF8HEzMaw3sLu8: 0
+```
+
--- homework-3/1529/index.js
+++ homework-3/1529/index.js
@@ -1,6 +1,55 @@
-// index.js
 const { ApiPromise, WsProvider } = require('@polkadot/api');
 const { mnemonicGenerate, mnemonicToMiniSecret, ed25519PairFromSeed, encodeAddress } = require('@polkadot/util-crypto');
+const { Keyring } = require('@polkadot/keyring');
+
+async function createAccount() {
+  const mnemonic = mnemonicGenerate();
+  const seed = mnemonicToMiniSecret(mnemonic);
+  const keypair = ed25519PairFromSeed(seed);
+  const address = encodeAddress(keypair.publicKey);
+
+  console.log(`助记词: ${mnemonic}`);
+  console.log(`地址: ${address}`);
+}
+
+async function checkBalance(api, address) {
+  if (!address) {
+    console.error('请提供一个地址');
+    return;
+  }
+
+  const { data: { free } } = await api.query.system.account(address);
+  console.log(`${address} 的余额: ${free.toHuman()}`);
+}
+
+async function transfer(api, fromAddress, toAddress, amount) {
+  if (!fromAddress || !toAddress || !amount) {
+    console.error('请提供发送方地址、接收方地址和转账金额');
+    return;
+  }
+
+  // 注意: 这里需要安全地获取发送方的私钥或助记词
+  const keyring = new Keyring({ type: 'sr25519' });
+  const sender = keyring.addFromUri('scout crystal produce repeat sun issue burger embody balance manage vibrant prefer');
+
+  try {
+    // 检查 API 结构
+    if (!api.tx.balances || typeof api.tx.balances.transferKeepAlive !== 'function') {
+        console.error('API 结构不正确，无法找到转账函数');
+        console.log('可用的模块:', Object.keys(api.tx));
+        if (api.tx.balances) {
+        console.log('balances 模块中的可用函数:', Object.keys(api.tx.balances));
+        }
+        return;
+    }
+    
+    const transfer = api.tx.balances.transferKeepAlive(toAddress, amount);
+    const hash = await transfer.signAndSend(sender);
+    console.log(`转账交易已提交，交易哈希: ${hash.toHex()}`);
+  } catch (error) {
+    console.error('转账失败:', error);
+  }
+}
 
 async function main() {
   const provider = new WsProvider('wss://rpc.polkadot.io');
@@ -9,28 +58,25 @@ async function main() {
   const args = process.argv.slice(2);
   const command = args[0];
 
-  if (command === 'create-account') {
-    const mnemonic = mnemonicGenerate();
-    const seed = mnemonicToMiniSecret(mnemonic);
-    const keypair = ed25519PairFromSeed(seed);
-    const address = encodeAddress(keypair.publicKey);
-
-    console.log(`Mnemonic: ${mnemonic}`);
-    console.log(`Address: ${address}`);
-  } else if (command === 'check-balance') {
-    const address = args[1];
-    if (!address) {
-      console.error('Please provide an address');
-      return;
+  try {
+    switch (command) {
+      case 'create-account':
+        await createAccount();
+        break;
+      case 'check-balance':
+        await checkBalance(api, args[1]);
+        break;
+      case 'transfer':
+        await transfer(api, args[1], args[2], args[3]);
+        break;
+      default:
+        console.log('无效命令。使用 "create-account"、"check-balance <地址>" 或 "transfer <发送方地址> <接收方地址> <金额>"。');
     }
-
-    const { data: { free } } = await api.query.system.account(address);
-    console.log(`Balance of ${address}: ${free.toHuman()}`);
-  } else {
-    console.log('Invalid command. Use "create-account" or "check-balance <address>".');
+  } catch (error) {
+    console.error('操作失败:', error);
+  } finally {
+    await api.disconnect();
   }
-
-  await api.disconnect();
 }
 
 main().catch(console.error);
